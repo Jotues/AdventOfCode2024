6b2b4c8bb57f660ddd634375a2ef1bb656e49d1d
Jotues <jastarzec@gmail.com>
2024-12-01 10:41:31 +0100

    day 1

--- day01.cpp
+++ day01.cpp
@@ -9,12 +9,29 @@ using namespace std;
 Solution::Solution(const std::string& input)
 {
     auto lines = utils::getLinesOfFile(input);
+    std::vector<int> first;
+    std::vector<int> second;
     
     for (const auto& line: lines)
     {
-
+        auto split = utils::splitStringByDelimeter(line, "  ");
+        first.push_back(stoi(split[0]));
+        second.push_back(stoi(split[1]));
     }
-    
+
+    std::ranges::sort(first);
+    std::ranges::sort(second);
+
+    for (auto i = 0; i < first.size(); i++)
+    {
+        resultPart1 += abs(first[i] - second[i]);
+    }
+
+    for (const auto e: first)
+    {
+        resultPart2 += e * std::ranges::count_if(second, [e](int secondE) { return secondE == e;});
+    }
+
     printResult(input);
 }
 
